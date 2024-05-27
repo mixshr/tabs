31009bcc9aa4cd3b81985df55d7c0cdda6e4d6f7
mihailsarenko <mixail.sharenko@gmail.com>
2024-05-27 13:37:55 +0500

    seconf commit

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,30 @@ import { FaAngleDoubleRight } from 'react-icons/fa'
 // I SWITCHED TO PERMANENT DOMAIN
 const url = 'https://course-api.com/react-tabs-project'
 function App() {
+  const [isLoading, setIsLoading] = useState(true)
+  const [jobs, setJobs] = useState([])
+
+  const fetchJobs = async () => {
+    try {
+      const response = await fetch(url);
+      const newJobs = response.json();
+      setJobs(newJobs);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  useEffect(() => { fetchJobs() }, []);
+  console.log(jobs);
+
+  if (isLoading) {
+    return <section className='jobs-center'>
+      <div className='loading'>
+        <h2>Loading...</h2>
+      </div>
+    </section>
+  }
+
   return <h2>tabs project setup</h2>
 }
 
